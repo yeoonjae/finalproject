7531d64d2c76de814e620d42f8eed7fca0efdc4b
jeeelung <jeeelung2@gmail.com>
2020-09-01 00:36:46 +0900

    마일리지 daoImpl

--- finalproject/src/main/java/com/kh/finalproject/repository/PointDaoImpl.java
+++ finalproject/src/main/java/com/kh/finalproject/repository/PointDaoImpl.java
@@ -1,5 +1,7 @@
 package com.kh.finalproject.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -17,6 +19,33 @@ public class PointDaoImpl implements PointDao{
 		int point_no = sqlSession.selectOne("point.getSeq");
 		pointDto.setPoint_no(point_no);
 		sqlSession.insert("point.regist", pointDto);
+	}
+
+	@Override
+	public List<PointDto> getList(String point_type) {
+		return sqlSession.selectList("point.getList2", point_type);
+	}
+
+	@Override
+	public List<PointDto> getList() {
+		return sqlSession.selectList("point.getList");
+	}
+
+	@Override
+	public PointDto get(int point_no) {
+		return sqlSession.selectOne("point.get", point_no);
+	}
+
+	@Override
+	public void edit(PointDto pointDto) {
+		sqlSession.update("point.edit", pointDto);
+		
+	}
+
+	@Override
+	public void delete(int point_no) {
+		sqlSession.delete("point.delete", point_no);
+		
 	}
 
 }
