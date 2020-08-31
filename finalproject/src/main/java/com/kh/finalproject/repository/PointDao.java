f34846ae4c8cb28de3b82769da721edeae48177f
jeeelung <jeeelung2@gmail.com>
2020-09-01 00:36:53 +0900

    마일리지 dao

--- finalproject/src/main/java/com/kh/finalproject/repository/PointDao.java
+++ finalproject/src/main/java/com/kh/finalproject/repository/PointDao.java
@@ -1,9 +1,15 @@
 package com.kh.finalproject.repository;
 
+import java.util.List;
+
 import com.kh.finalproject.entity.PointDto;
 
 public interface PointDao {
 
 	void regist(PointDto pointDto);
-	
+	List<PointDto> getList();
+	List<PointDto> getList(String point_type);
+	PointDto get(int point_no);
+	void edit(PointDto pointDto);
+	void delete(int point_no);
 }
