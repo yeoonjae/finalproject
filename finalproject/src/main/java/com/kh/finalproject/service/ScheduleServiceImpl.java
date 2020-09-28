cc1753ed3fe349307ca18ab25abe838ef70299c8
Monday_moning <Monday_moning@ohhanjun>
2020-09-28 18:44:07 +0900

    스케쥴러 삭제

--- finalproject/src/main/java/com/kh/finalproject/service/ScheduleServiceImpl.java
+++ finalproject/src/main/java/com/kh/finalproject/service/ScheduleServiceImpl.java
@@ -26,53 +26,53 @@ public class ScheduleServiceImpl implements ScheduleService{
 	private MemberCouponDao memberCouponDao;
 	
 	// 매일 자정마다 시작일이 오늘인 쿠폰을 회원쿠폰함에 등록
-	@Scheduled(cron="0 0 0 * * *") // 매일 자정마다
-	@Override
-	public void regist() {
-		// 시작일이 오늘인 쿠폰 조회
-		List<CouponDto> couponList = couponDao.todayStart();
-		
-		// 회원 쿠폰 내역 테이블에 등록
-		MemberCouponDto memberCouponDto;
-		for(int i=0; i<couponList.size(); i++) {
-			memberCouponDto = MemberCouponDto.builder()
-								.coupon_no(couponList.get(i).getCoupon_no())
-								.branch_no(couponList.get(i).getBranch_no())
-								.member_no(couponList.get(i).getMember_no())
-								.build();
-			memberCouponDao.regist(memberCouponDto);
-		}
-		
-	}
-
-	// 매일 23시 59분마다 종료일이 오늘인 쿠폰을 회원쿠폰함에서 삭제
-	@Scheduled(cron="0 59 23 * * *") // 매일 23:59 마다
-	@Override
-	public void delete() {
-		// 종료일이 오늘인 쿠폰 조회
-		List<CouponDto> couponList = couponDao.todayFinish();
-		
-		// 쿠폰 내역에서 삭제
-		for(int i=0; i<couponList.size(); i++) {
-			int coupon_no = couponList.get(i).getCoupon_no();
-			memberCouponDao.delete(coupon_no);
-		}
-		
-	}
-
-	@Scheduled(cron="0 58 23 * * *") // 매일 23:58 마다
-	@Override
-	public void changeState() {
-		// 시작일이 내일인 대기중인 쿠폰요청 조회
-		List<Integer> noList = couponReqDao.changeStateList();
-		
-		// 해당 쿠폰 '거절'로 변경
-		for(int i=0; i<noList.size(); i++) {
-			int coupon_req_no = noList.get(i);
-			couponReqDao.changeState(coupon_req_no);
-		}
-		
-	}
+//	@Scheduled(cron="0 0 0 * * *") // 매일 자정마다
+//	@Override
+//	public void regist() {
+//		// 시작일이 오늘인 쿠폰 조회
+//		List<CouponDto> couponList = couponDao.todayStart();
+//		
+//		// 회원 쿠폰 내역 테이블에 등록
+//		MemberCouponDto memberCouponDto;
+//		for(int i=0; i<couponList.size(); i++) {
+//			memberCouponDto = MemberCouponDto.builder()
+//								.coupon_no(couponList.get(i).getCoupon_no())
+//								.branch_no(couponList.get(i).getBranch_no())
+//								.member_no(couponList.get(i).getMember_no())
+//								.build();
+//			memberCouponDao.regist(memberCouponDto);
+//		}
+//		
+//	}
+//
+//	// 매일 23시 59분마다 종료일이 오늘인 쿠폰을 회원쿠폰함에서 삭제
+//	@Scheduled(cron="0 59 23 * * *") // 매일 23:59 마다
+//	@Override
+//	public void delete() {
+//		// 종료일이 오늘인 쿠폰 조회
+//		List<CouponDto> couponList = couponDao.todayFinish();
+//		
+//		// 쿠폰 내역에서 삭제
+//		for(int i=0; i<couponList.size(); i++) {
+//			int coupon_no = couponList.get(i).getCoupon_no();
+//			memberCouponDao.delete(coupon_no);
+//		}
+//		
+//	}
+//
+//	@Scheduled(cron="0 58 23 * * *") // 매일 23:58 마다
+//	@Override
+//	public void changeState() {
+//		// 시작일이 내일인 대기중인 쿠폰요청 조회
+//		List<Integer> noList = couponReqDao.changeStateList();
+//		
+//		// 해당 쿠폰 '거절'로 변경
+//		for(int i=0; i<noList.size(); i++) {
+//			int coupon_req_no = noList.get(i);
+//			couponReqDao.changeState(coupon_req_no);
+//		}
+//		
+//	}
 
 	@Autowired
 	private SqlSession sqlSession;
