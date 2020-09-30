9e8d549ae4053cbd053b851ddad95fe4516d919f
duswo5310"duswoWkd1@ <duswo5310@gmail.com>
2020-09-30 11:19:47 +0900

    회원이랑 관리자 interceptor등록

---
+++ finalproject/src/main/java/com/kh/finalproject/interceptor/MemberInterceptor.java
@@ -0,0 +1,28 @@
+package com.kh.finalproject.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.kh.finalproject.entity.MemberDto;
+
+public class MemberInterceptor extends HandlerInterceptorAdapter{
+	@Autowired
+	HttpSession session;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		//회원이 로그인 되어있으면
+		MemberDto memberDto = (MemberDto)session.getAttribute("memberinfo");
+		if(memberDto != null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
